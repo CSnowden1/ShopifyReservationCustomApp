5679975d894eadf158667619c9c96a168c0fc23d
CSnowden1 <christopher@fancyawesome.com>
2024-04-24 14:23:59 -0400

    Update cartUpdateRoutes.js

--- src/routes/cartUpdateRoutes.js
+++ src/routes/cartUpdateRoutes.js
@@ -28,9 +28,9 @@ function verifyWebhook(data, hmacHeader) {
 router.post('/carts-update', (req, res) => {
     try {
         // Log the headers and body for debugging
-        console.log('Headers:', req.headers);
         console.log('Body:', req.body);
-        res.status(200).send('Item Added to a Cart', res);
+        console.log('Cart Id:', req.body.id);
+        res.status(200).send('Item Added to a Cart');
     } catch (error) {
         console.error('Error processing webhook:', error.message);
         // Respond with a server error status code and message
