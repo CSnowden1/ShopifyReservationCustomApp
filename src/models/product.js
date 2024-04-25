f7f8307e41b7d87516f2fcc04c460428d2a7b5b1
CSnowden1 <christopher@fancyawesome.com>
2024-04-25 17:30:51 -0400

    Update product.js

--- src/models/product.js
+++ src/models/product.js
@@ -1,31 +1,5 @@
 const mongoose = require('mongoose');
 
-const variantSchema = new mongoose.Schema({
-    productId: {
-            type: String,
-            required: true,
-            unique: true
-    },
-  variantId: {
-    type: String, // Variant ID from Shopify
-    required: true,
-    unique: true
-  },
-  title: {
-    type: String,
-    required: true
-  },
-  inventoryCount: {
-    type: Number,
-    required: true,
-    min: [0, 'Inventory cannot be negative']
-  },
-  reservationDuration: {
-    type: Number, // Duration in minutes
-    required: true
-  }
-});
-
 const productSchema = new mongoose.Schema({
   productId: {
     type: String, // Product ID from Shopify
@@ -44,8 +18,7 @@ const productSchema = new mongoose.Schema({
   description: {
     type: String,
     required: false
-  },
-  variants: [variantSchema] // Array of variant sub-documents
+  }
 });
 
 const Product = mongoose.model('Product', productSchema);
